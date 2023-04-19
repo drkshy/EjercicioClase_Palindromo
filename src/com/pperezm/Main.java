3b435a7d1c7525343b15e44f7fe622d9c16f8fc1
Pablo Arturo Perez Mata <pablo.arturo.perez@gmail.com>
2023-04-19 17:30:34 -0600

    Initial commit

---
+++ src/com/pperezm/Main.java
@@ -0,0 +1,40 @@
+package com.pperezm;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String cadena ="somos";
+        ArrayDeque <Character> pila = new ArrayDeque<>();
+        boolean esPalindromo = true;
+	// write your code here
+        for(int i=0; i< cadena.length();i++){
+            pila.push(cadena.charAt(i));
+           //System.out.println(cadena.charAt(i));
+
+        }
+
+        for(int j=0; j< cadena.length();j++) {
+            cadena.charAt(j);
+            //System.out.println("");
+            //System.out.println(cadena.charAt(j));
+            //System.out.println(j);
+
+            if (cadena.charAt(j) == pila.pop()) {
+                continue;
+
+            } else {
+                esPalindromo = false;
+                break;
+            }
+
+        }
+
+        String mensaje = esPalindromo?"Si es palindromo":"No es palindromo";
+        System.out.println(cadena);
+            System.out.println(mensaje);
+
+        }
+    }
+
+
